1de3545152c7b5d19fc41db06038d4ea04696a10
Bryan Steve Jiménez Chacón <bsjc96@gmail.com>
2018-09-09 21:53:13 -0600

    Creacion seccion para proyecto 3

---
+++ Proyecto 3/P3/src/lib/matrix.h
@@ -0,0 +1,4 @@
+int ** createMatrix(int rows, int columns);
+void printMatrix(int **matrix, int rows, int columns);
+float** createFloatMatrix(int rows, int columns);
+void printFloatMatrix(float **matrix, int rows, int columns);
